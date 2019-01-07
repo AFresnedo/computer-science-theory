52af4063f3df5e15beb4183016d760d91e363a46
AFresnedo <AFresnedo@users.noreply.github.com>
2019-01-07 02:47:27 -0800

    add design notes for Node.java in java bst folder

--- trees/bst/java/Node.java
+++ trees/bst/java/Node.java
@@ -1,5 +1,13 @@
 package myTrees;
 
+/* Possible design: a node with variable links (a graph node)... data
+ * structures could choose what setup they want in a generic fashion (so bst
+ * would create a node with 2 links...left and right child)...in that case,
+ * links would probably be implemented as (array or linked list? array... when
+ * a node is created the links will be fixed and each may have a specific
+ * purpose so 0 could either be null or left child, 1 could be null or right
+ * child, 2 could be null or parent...etc) */
+
 class Node<T extends Comparable<? super T>> {
 
     T val;
