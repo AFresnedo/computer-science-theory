8de936bf483e6a0c1cfa86a8c78279bbabad0ee6
AFresnedo <AFresnedo@users.noreply.github.com>
2019-01-22 12:40:39 -0800

    setup maven-trees pom to run both junit5 and junit4 tests

---
+++ trees/java/maven-trees/src/test/java/io/github/afresnedo/trees/FirstJUnit5Tests.java
@@ -0,0 +1,14 @@
+package io.github.afresnedo.trees;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstJUnit5Tests {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
