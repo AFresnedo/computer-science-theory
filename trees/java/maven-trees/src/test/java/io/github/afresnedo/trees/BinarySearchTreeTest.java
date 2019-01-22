509dc539df154e378d89b73049fd0a95bb697bf1
AFresnedo <AFresnedo@users.noreply.github.com>
2019-01-22 15:21:47 -0800

    code constructor unit test for java BST

--- trees/java/maven-trees/src/test/java/io/github/afresnedo/trees/BinarySearchTreeTest.java
+++ trees/java/maven-trees/src/test/java/io/github/afresnedo/trees/BinarySearchTreeTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Binary Search Tree Unit Test")
 class BinarySearchTreeTest {
@@ -11,7 +12,24 @@ class BinarySearchTreeTest {
     @Test
     @DisplayName("Constructor creates a tree with lambda deep copy")
     void testConstructor() {
-        assertFalse(false);
+
+        // Integer data type
+        BinarySearchTree<Integer> ints =
+            new BinarySearchTree<Integer>(5, x -> x);
+        assertTrue(ints.contains(5));
+        assertFalse(ints.contains(1));
+
+        // String data type
+        BinarySearchTree<String> strs =
+            new BinarySearchTree<String>("hello", x -> x);
+        assertTrue(strs.contains("hello"));
+        assertFalse(strs.contains("goodbye"));
+
+        // Deep copier that turns everything into null
+        BinarySearchTree<Integer> zeroNodes =
+            new BinarySearchTree<Integer>(5, x -> 0);
+        assertTrue(zeroNodes.contains(0));
+        assertFalse(zeroNodes.contains(5));
     }
 
     @Test
