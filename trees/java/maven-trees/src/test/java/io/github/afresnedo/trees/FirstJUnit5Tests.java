6ee192dd536d25ed105975255369ef64cac7a218
AFresnedo <AFresnedo@users.noreply.github.com>
2019-01-22 13:08:30 -0800

    write unit test skeleton for java bst

--- trees/java/maven-trees/src/test/java/io/github/afresnedo/trees/FirstJUnit5Tests.java
+++ trees/java/maven-trees/src/test/java/io/github/afresnedo/trees/FirstJUnit5Tests.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class FirstJUnit5Tests {
+class FirstJUnit5Tests {
 
     @Test
     void myFirstTest() {
---
+++ trees/java/maven-trees/src/test/java/io/github/afresnedo/trees/OldBinarySearchTreeTest.java
@@ -0,0 +1,78 @@
+package io.github.afresnedo.trees;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.util.LinkedList;
+
+/**
+ * Unit test for BST.
+ */
+public class OldBinarySearchTreeTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public OldBinarySearchTreeTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( OldBinarySearchTreeTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testBinarySearchTree()
+    {
+
+        // Instantiate tree
+        BinarySearchTree<Integer> testOne =
+            new BinarySearchTree<Integer>(x -> x);
+
+        testOne.insert(35);
+        testOne.insert(999);
+
+        // 35 should be in the tree
+        assertTrue(testOne.contains(35));
+        // 999 should be in the tree
+        assertTrue(testOne.contains(999));
+        // 1 should not be found
+        assertFalse(testOne.contains(1));
+
+
+        // // Build test list
+        // LinkedList<Integer> list = new LinkedList<Integer>();
+        // list.addLast(35);
+        // list.addLast(15);
+        // list.addLast(45);
+        // list.addLast(5);
+        // list.addLast(25);
+        // list.addLast(1);
+        // list.addLast(10);
+        // list.addLast(20);
+        // list.addLast(30);
+        // list.addLast(29);
+        // list.addLast(32);
+        // list.addLast(31);
+        // list.addLast(45);
+        // list.addLast(50);
+        // list.addLast(40);
+
+        // // Build test tree original state
+        // BinarySearchTree<Integer> original =
+            // new BinarySearchTree<Integer>(x -> x);
+        // original.insertBulk(list);
+
+    }
+}
