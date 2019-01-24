ba1f4e0bc0e601f6ea49f0bcd9314d9657b6a9fe
AFresnedo <AFresnedo@users.noreply.github.com>
2019-01-24 10:48:30 -0800

    begin writing binary node for python trees

---
+++ trees/python/binode.py
@@ -0,0 +1,9 @@
+class BinaryNode:
+  def __init__(self, val = None, left = None, right = None):
+    self.val = val;
+    self.left = left;
+    self.right = right;
+
+firstNode = BinaryNode(0, None, None);
+
+secondNode = BinaryNode();
