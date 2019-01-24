9cc6027d63d64a32cdd8988e6c4952231d3bec4e
AFresnedo <AFresnedo@users.noreply.github.com>
2019-01-24 10:59:31 -0800

    add type checking for BinaryNode in left, right fields for BinaryNode

--- trees/python/binode.py
+++ trees/python/binode.py
@@ -1,5 +1,9 @@
 class BinaryNode:
   def __init__(self, val = None, left = None, right = None):
+    if not isinstance(left, BinaryNode) and left is not None:
+        raise TypeError('left link should be a BinaryNode type')
+    if not isinstance(right, BinaryNode) and right is not None:
+        raise TypeError('right link should be a BinaryNode type')
     self.val = val;
     self.left = left;
     self.right = right;
